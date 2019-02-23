50cef2ddc32c9c574e8287bc75b9ba396f1179ba
Utkarsh <srivastavautkarsh8097@gmail.com>
2019-02-23 18:27:48 +0530

    Bug - Docs were not opening due to invalid path

--- js/main.js
+++ js/main.js
@@ -1,66 +1,64 @@
-const controller = (function(){
+const controller = (function () {
 
     let pdfDoc = null,
-    pageNum = 1,
-    pageIsRendering = false,
-    pageNumIsPending = null,
-    scale = 2,
-    url = '../Docs/Beginning Node.js.pdf';
+        pageNum = 1,
+        pageIsRendering = false,
+        pageNumIsPending = null,
+        scale = 2,
+        url = '../Docs/Beginning Node.js.pdf';
     const canvas = document.querySelector('#pdf-render'),
         ctx = canvas.getContext('2d');
 
-    const GetURL = function(link){
+    const GetURL = function (link) {
         url = link;
     }
 
     const setupEventListeners = () => {
         // For Previous page
         document.getElementById('prev-page').addEventListener('click', displayPDFfuncs().onPrevPage);
-        document.addEventListener('keydown', function(event){
-            if(event.keyCode === 37 || event.which === 37)
+        document.addEventListener('keydown', function (event) {
+            if (event.keyCode === 37 || event.which === 37)
                 displayPDFfuncs().onPrevPage();
         });
 
         // For Next page
         document.getElementById('next-page').addEventListener('click', displayPDFfuncs().onNextPage);
-        document.addEventListener('keydown', function(event){
-            if(event.keyCode === 39 || event.which === 39)
+        document.addEventListener('keydown', function (event) {
+            if (event.keyCode === 39 || event.which === 39)
                 displayPDFfuncs().onNextPage();
         });
 
         // For scale adjustment
         document.querySelector('.scale-btn').addEventListener('click', setScale);
-        document.querySelector('.scale').addEventListener('keypress', function(event){
-            if(event.keyCode === 13 || event.which === 13)
-            {
+        document.querySelector('.scale').addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
                 setScale();
             }
         });
 
         // For Navigation
-        document.getElementById('page-num').addEventListener('keypress', function(event){
-            if(event.keyCode === 13 || event.which === 13)
-            {
+        document.getElementById('page-num').addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
                 navigate();
             }
         });
     }
 
-    const setScale = function(){
+    const setScale = function () {
         scale = Number(document.querySelector('.scale').value);
         displayPDF(displayPDFfuncs().renderPage);
         // Clear the field
         document.querySelector('.scale').value = '';
     }
 
-    const displayPDFfuncs = function() {
+    const displayPDFfuncs = function () {
         // Render the page
         const renderPage = num => {
             pageIsRendering = true;
 
             // Get the page 
             pdfDoc.getPage(num).then(page => {
-                const viewport = page.getViewport({ scale: scale});
+                const viewport = page.getViewport({ scale: scale });
                 canvas.height = viewport.height;
                 canvas.width = viewport.width;
 
@@ -72,7 +70,7 @@ const controller = (function(){
                 page.render(renderCtx).promise.then(pdfDoc_ => {
                     pageIsRendering = false;
 
-                    if(pageNumIsPending !== null){
+                    if (pageNumIsPending !== null) {
                         renderPage(pageNumIsPending);
                         pageNumIsPending = null;
                     }
@@ -87,25 +85,25 @@ const controller = (function(){
 
         function queueRenderPage(num) {
             if (pageIsRendering) {
-              pageNumPending = num;
+                pageNumPending = num;
             } else {
-              renderPage(num);
+                renderPage(num);
             }
         };
 
         //Display previous page
         function onPrevPage() {
             if (pageNum <= 1) {
-              return;
+                return;
             }
             pageNum--;
             queueRenderPage(pageNum);
         };
-        
+
         // Display next page
         function onNextPage() {
             if (pageNum >= pdfDoc.numPages) {
-              return;
+                return;
             }
             pageNum++;
             queueRenderPage(pageNum);
@@ -122,25 +120,25 @@ const controller = (function(){
     const displayPDF = (renderPage) => {
         // Get the document
         pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
-            pdfDoc = pdfDoc_;        
+            pdfDoc = pdfDoc_;
             document.querySelector('#page-count').textContent = pdfDoc.numPages;
             renderPage(pageNum);
         });
     }
 
-    const navigate = function(){
+    const navigate = function () {
         pageNum = Number(document.getElementById('page-num').value);
-        if (pageNum && pageNum <= pdfDoc.numPages && pageNum >= 1){
+        if (pageNum && pageNum <= pdfDoc.numPages && pageNum >= 1) {
             displayPDFfuncs().queueRenderPage(pageNum);
         }
-        else{
+        else {
             document.getElementById('page-num').value = '';
         }
-    }    
+    }
 
     return {
-        initate: function(){
-            GetURL('../Docs/' + window.location.href.split('=')[1].replace('+', ' '));
+        initate: function () {
+            GetURL('../Docs/' + window.location.href.split('=')[1].split('+').join(' '));
             setupEventListeners();
             displayPDF(displayPDFfuncs().renderPage);
         }
